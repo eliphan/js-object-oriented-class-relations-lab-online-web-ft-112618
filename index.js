ccc3e382c5ec0685613000b3436c03592fba3eee
Elise Phan <aliceconnic@gmail.com>
2019-03-17 00:23:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,13 +12,13 @@ class Driver {
     this.name = name;
     store.drivers.push(this)
   }
+  
   trips() {
     return store.trips.filter(
         function(trip) {
             return trip.driverId === this.id;
               }.bind(this)
-    );
-  }
+    )}
 }
 
 let passengerId = 0
